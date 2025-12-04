19aaa40402bec5207732e59e8724a95ef865f2b4
Your Name <you@example.com>
2025-12-04 14:45:52 +0530

    hj

---
+++ server.js
@@ -0,0 +1,10 @@
+// server/server.js (use this exactly)
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
